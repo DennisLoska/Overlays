d73d9626aa259d9e2356c9ebe2427adc504b7b94
DennisLoska <linksy@hotmail.de>
2018-06-10 20:15:51 +0200

    debugging ImageGenerator

--- src/js/ImageGenerator.js
+++ src/js/ImageGenerator.js
@@ -4,25 +4,33 @@ class ImageGenerator {
         this.width = 150;
         this.height = 150;
 
-        this.randomImagePixels = new Array(this.width * this.height);
+        this.rndImagePixels = new Array(this.width * this.height);
 
         // generate color between 1 and 255
         var randomR = Math.floor((Math.random() * 255) + 1);
         var randomG = Math.floor((Math.random() * 255) + 1);
         var randomB = Math.floor((Math.random() * 255) + 1);
 
-        for (var i = 0; i < this.randomImagePixels.length; i+=4){
-            this.randomImagePixels[i+0] = randomR; // R
-            this.randomImagePixels[i+1] = randomG; // G
-            this.randomImagePixels[i+2] = randomB; // B
-            this.randomImagePixels[i+3] = 255; // A
-         }
+        for (var i = 0; i < this.rndImagePixels.length; i += 4) {
+            this.rndImagePixels[i + 0] = randomR; // R
+            this.rndImagePixels[i + 1] = randomG; // G
+            this.rndImagePixels[i + 2] = randomB; // B
+            this.rndImagePixels[i + 3] = 255; // A
+        }
 
-	}
+    }
 
-	get randomImagePixels() {
+    set randomImagePixels(array) {
+        this.rndImagePixels = array
+    }
+
+    get randomImagePixels() {
         // retrun the pixel array for a random image, use this array in Images() class
-		return this.randomImagePixels;
-	}
+        return this.rndImagePixels;
+    }
 
 }
+
+//left for debugging purposes
+//var test = new ImageGenerator();
+//console.log(test.randomImagePixels);
