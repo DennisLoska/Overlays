383db795080432938eb7ed1f3d7b4eefaeefbb71
Luisa Kurth <s0559659@htw-berlin.de>
2018-07-22 23:02:45 +0200

    comments

--- src/js/game/Level.js
+++ src/js/game/Level.js
@@ -115,10 +115,11 @@ class Level {
            doGenerate = basis / target image auswahl
            gray = generierte bilder in graustufen
            empty = ein bild soll leer sein 
+           similarShapes = formen werden an ähnlichen positionen gemalt
            folderImage = benutze bild aus ordner anstatt vom generator */
 
         this.levelSettings[0] = [3, 2, false, false, false, false, false]
-        this.levelSettings[1] = [4, 2, false, false, false, false, false]
+        this.levelSettings[1] = [4, 2, false, false, false, true, false]
         this.levelSettings[2] = [4, 3, false, false, false, false, false]
         this.levelSettings[3] = [5, 2, false, false, false, false, false]
         this.levelSettings[4] = [5, 3, false, false, false, false, false]
